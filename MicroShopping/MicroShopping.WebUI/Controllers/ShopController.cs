c641f72834a8111227d601ac6fe21524b9f7147f
Sergio Tapia Gutierrez <sergiotapia@outlook.com>
2012-09-20 16:15:14 -0400

    Wired up the checkout pages. Now to integrate Stripe.

--- MicroShopping/MicroShopping.WebUI/Controllers/ShopController.cs
+++ MicroShopping/MicroShopping.WebUI/Controllers/ShopController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MicroShopping.Domain.Abstract;
+using MicroShopping.WebUI.Models;
+using AutoMapper;
+using MicroShopping.Domain;
 
 namespace MicroShopping.WebUI.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly IPackageRepository packageRepository;
+
+        public ShopController(IPackageRepository _packageRepository)
+        {
+            packageRepository = _packageRepository;
+        }
+
         public ActionResult BuyLances()
         {
             return View();
@@ -16,9 +27,10 @@ namespace MicroShopping.WebUI.Controllers
         [HttpPost]
         public ActionResult CheckoutPackage()
         {
-            var packageId = Request.Form["packageId"];
-
-            return View();
+            var packageId = Convert.ToInt32(Request.Form["packageId"]);
+            var package = packageRepository.FindPackageById(packageId);
+            var model = Mapper.Map<LancePackage, LancePackageModel>(package);
+            return View(model);
         }
     }
 }
